43cadea94422c95965659de5ac1a3513434cda95
Andy Bradbury <andy@thestartupguys.co.uk>
2019-06-20 12:47:51 +0100

    get all form fields on page

---
+++ js/mainscript.js
@@ -0,0 +1,20 @@
+window.addEventListener('load', ()=> {
+    //
+    // Variables
+    //
+    var allFields = Array.from(document.querySelectorAll(".tip-calculator input"));
+
+    //
+    // Methods
+    //
+
+});
+
+/*
+ * Things to do:
+ * - Get all of the fields.
+ * - Listen for form submissions.
+ * - Calculate the tip as a percentage of the total bill.
+ * - Add the tip to the total.
+ * - Calculate the per-person amount by dividing the total with tip among the total number of people.
+ */
